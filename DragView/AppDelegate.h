c1c2e624f8ecaea3c33a675dbf5494048454200c
fromai <formai@fromaideimac.local>
2017-06-19 14:04:59 +0800

    Initial Commit

---
+++ DragView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DragView
+//
+//  Created by fromai on 2017/6/19.
+//  Copyright © 2017年 Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
